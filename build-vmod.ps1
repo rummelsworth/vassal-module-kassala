af3dce6bdbf2bd90e6a3436e5c494a17ef192218
William Rummler <w.a.rummler@gmail.com>
2022-02-02 20:13:31 -0600

    Use git & date metadata in build filename.

--- build-vmod.ps1
+++ build-vmod.ps1
@@ -1,3 +1,6 @@
-$vmod = ".\Kassala.vmod"
+$tag = git tag --points-at HEAD
+$sha = git rev-parse --short HEAD
+$timestamp = (Get-Date).ToString("yyyyMMddHHmmssfff")
+$vmod = ".\Kassala_$tag+${sha}_$timestamp.vmod"
 Remove-Item -ErrorAction Ignore "$vmod"
 [System.IO.Compression.ZipFile]::CreateFromDirectory(".\vmod", $vmod)
