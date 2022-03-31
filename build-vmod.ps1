52940425f97edb383e20a1524aebb99feb5ebb52
William Rummler <w.a.rummler@gmail.com>
2022-03-30 22:54:20 -0500

    WIP build script watch mode.

--- build-vmod.ps1
+++ build-vmod.ps1
@@ -1,6 +1,44 @@
+param([switch] $NoWatch)
+
 $tag = git tag --points-at HEAD
 $sha = git rev-parse --short HEAD
 $timestamp = (Get-Date).ToString("yyyyMMddHHmmssfff")
-$vmod = ".\Kassala_$tag+${sha}_$timestamp.vmod"
-Remove-Item -ErrorAction Ignore "$vmod"
-[System.IO.Compression.ZipFile]::CreateFromDirectory(".\vmod", $vmod)
+$vmodName = "Kassala_$tag+${sha}_$timestamp.vmod"
+$vmodPath = ".\$vmodName"
+Remove-Item -ErrorAction Ignore "$vmodPath"
+[IO.Compression.ZipFile]::CreateFromDirectory(".\vmod", $vmodPath)
+Write-Host "VMOD file built: $vmodPath"
+
+if ($NoWatch)
+{
+    Exit
+}
+
+$watcher = New-Object System.IO.FileSystemWatcher ".", $vmodName -Property @{
+    NotifyFilter = [IO.NotifyFilters]::LastWrite
+}
+
+Register-ObjectEvent $watcher -EventName Changed -Action {
+    Write-Host "VMOD file change detected."
+    & ".\unbuild-vmod.ps1" $Event.SourceEventArgs.FullPath
+} >$null
+
+$watcher.EnableRaisingEvents = $true
+
+Write-Host "Press Ctrl+C to stop watching VMOD build output." -ForegroundColor ([ConsoleColor]::Yellow)
+
+while ($true)
+{
+    if ([Console]::KeyAvailable)
+    {
+        $keyInfo = [System.Console]::ReadKey($true)
+        if ($keyInfo.Modifiers -eq [System.ConsoleModifiers]::Control -and $keyInfo.Key -eq [System.ConsoleKey]::C)
+        {
+            Exit
+        }
+    }
+    else
+    {
+        Start-Sleep 0.5
+    }
+}
