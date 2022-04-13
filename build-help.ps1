53d5c6fb22d43493d7d519278976d3737bed093f
William Rummler <w.a.rummler@gmail.com>
2022-04-12 21:06:28 -0500

    Add watch mode for help build script.

--- build-help.ps1
+++ build-help.ps1
@@ -1,1 +1,37 @@
-pandoc .\rules.md -o help\rules.html -s --citeproc --bibliography .\The_Complete_Book_of_Wargames.bibtex
+param([switch] $NoWatch)
+
+$build = {
+    pandoc .\rules.md -o .\help\rules.html -s --citeproc --bibliography .\The_Complete_Book_of_Wargames.bibtex
+    Write-Host "Help build complete."
+}
+
+& $build
+
+if ($NoWatch)
+{
+    exit
+}
+
+$watcher = New-Object IO.FileSystemWatcher ".", "rules.md" -Property @{
+    NotifyFilter = [IO.NotifyFilters]::LastWrite
+}
+
+$watcherEventSourceId = [guid]::NewGuid()
+$watcherEventJob = Register-ObjectEvent $watcher -SourceIdentifier $watcherEventSourceId -EventName Changed -MessageData $build -Action {
+    Write-Host "Help source file change detected."
+    & $Event.MessageData
+}
+
+$watcher.EnableRaisingEvents = $true
+
+Write-Host "Press Ctrl+C to stop watching help source file." -ForegroundColor ([ConsoleColor]::Yellow)
+
+try
+{
+    Wait-Event -SourceIdentifier $watcherEventSourceId
+}
+finally
+{
+    $null = while ([Console]::KeyAvailable) { [Console]::ReadKey($true) }
+    $watcherEventJob | Remove-Job -Force
+}
