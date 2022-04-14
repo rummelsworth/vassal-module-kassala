dd64cf34cfac6e392b208516dc623576c05bac2b
William Rummler <w.a.rummler@gmail.com>
2022-04-14 08:12:57 -0500

    Extract watch-vmod script.

--- build-vmod.ps1
+++ build-vmod.ps1
@@ -1,39 +1,16 @@
-param([switch] $NoWatch)
+param([switch] $noWatch)
 
 $tag = git tag --points-at HEAD
 $sha = git rev-parse --short HEAD
 $timestamp = (Get-Date).ToString("yyyyMMddHHmmssfff")
-$vmodName = "Kassala_$tag+${sha}_$timestamp.vmod"
-$vmodPath = ".\$vmodName"
+$vmodPath = ".\Kassala_$tag+${sha}_$timestamp.vmod"
 Remove-Item -ErrorAction Ignore "$vmodPath"
 [IO.Compression.ZipFile]::CreateFromDirectory(".\vmod", $vmodPath)
 Write-Host "VMOD file built: $vmodPath"
 
-if ($NoWatch)
+if ($noWatch)
 {
     exit
 }
 
-$watcher = New-Object IO.FileSystemWatcher ".", $vmodName -Property @{
-    NotifyFilter = [IO.NotifyFilters]::LastWrite
-}
-
-$watcherEventSourceId = [guid]::NewGuid()
-$watcherEventJob = Register-ObjectEvent $watcher -SourceIdentifier $watcherEventSourceId -EventName Changed -Action {
-    Write-Host "VMOD file change detected."
-    & ".\unbuild-vmod.ps1" $Event.SourceEventArgs.FullPath
-}
-
-$watcher.EnableRaisingEvents = $true
-
-Write-Host "Press Ctrl+C to stop watching VMOD build output." -ForegroundColor ([ConsoleColor]::Yellow)
-
-try
-{
-    Wait-Event -SourceIdentifier $watcherEventSourceId
-}
-finally
-{
-    $null = while ([Console]::KeyAvailable) { [Console]::ReadKey($true) }
-    $watcherEventJob | Remove-Job -Force
-}
+& ".\watch-vmod.ps1" $vmodPath
--- unbuild-vmod.ps1
+++ unbuild-vmod.ps1
@@ -1,6 +1,6 @@
-param([string] $Path)
+param([string] $vmodPath)
 
-if ([string]::IsNullOrWhiteSpace($Path))
+if ([string]::IsNullOrWhiteSpace($vmodPath))
 {
     Write-Error "Path to VMOD file is blank."
     Exit
@@ -8,5 +8,5 @@ if ([string]::IsNullOrWhiteSpace($Path))
 
 Remove-Item .\vmod -Recurse
 New-Item .\vmod -ItemType Directory >$null
-[IO.Compression.ZipFile]::ExtractToDirectory($Path, ".\vmod")
+[IO.Compression.ZipFile]::ExtractToDirectory($vmodPath, ".\vmod")
 Write-Host "VMOD file unbuilt."
